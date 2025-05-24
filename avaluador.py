ed0580295226b043845915c8181ddde18eb6b60b
VICTRX17 <1746098@uab.cat>
2025-05-24 16:48:28 +0200

    Create avaluador.py
    Està acabat ja

---
+++ avaluador.py
@@ -0,0 +1,32 @@
+import numpy as np
+from dataset import Dataset
+
+
+class Avaluador:
+    """
+    Classe per avaluar la qualitat de les recomanacions d'un sistema de recomanació.
+    Aquesta classe calcula l'error absolut mitjà (MAE) entre les valoracions reals i les recomanades.
+    Attributes:
+        valoracins_reals (np.ndarray): Matriu de valoracions reals.
+        dataset (Dataset): Instància del conjunt de dades utilitzat per a les recomanacions.
+        valoracions (np.ndarray): Matriu de valoracions del dataset.
+    """
+    def __init__(self, valoracins_reals, dataset): 
+        self._valoracins_reals = valoracins_reals # Matriu de valoracions reals
+        self._dataset = dataset
+        self._valoracions = dataset.get_valoracions()
+
+    def calcula_mae(self):
+        """
+        Aquesta funció calcula i retorna el valor (float) del
+        MAE (Mean Absolute Error), una mètrica per valorar la precisió de les prediccions.
+        """
+        return np.mean(np.abs(self._valoracins_reals - self._valoracions))
+    
+    def calcula_rmse(self):
+        """
+        Aquesta funció calcula i retorna el valorn (float) del
+        RMSE , una mètrica per valorar la precisió de les prediccions
+        """
+        mse = np.mean((self._valoracins_reals - self._valoracions) ** 2)
+        return np.sqrt(mse)
