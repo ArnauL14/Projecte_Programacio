dd022508e3619b2eb8684c69bdc33022e628ce05
ArnauL14 <1744876@uab.cat>
2025-05-25 14:43:52 +0200

    Finalització item.py
    Atributs privats

--- item.py
+++ item.py
@@ -1,27 +1,27 @@
 class Item:
     def __init__(self, id_item, titol):
-        self.id = id_item
-        self.titol = titol
+        self._id = id_item
+        self._titol = titol
 
     def __str__(self):
-        return f"{self.titol}"
+        return f"{self._titol}"
 
 
 class Pelicula(Item):
     def __init__(self, id_item, titol, generes):
         super().__init__(id_item, titol)
-        self.genere = generes  # llista de strings
+        self._genere = generes  # llista de strings
 
     def __str__(self):
-        return f"{self.titol} - {', '.join(self.genere)}"
+        return f"{self._titol} - {', '.join(self._genere)}"
 
 
 class Llibre(Item):
     def __init__(self, isbn, titol, autor, any_publicacio, editorial):
         super().__init__(isbn, titol)
-        self.autor = autor
-        self.any = any_publicacio
-        self.editorial = editorial
+        self._autor = autor
+        self._any = any_publicacio
+        self._editorial = editorial
 
     def __str__(self):
-        return f"{self.titol} ({self.any}) - {self.autor}, {self.editorial}"
+        return f"{self._titol} ({self._any}) - {self._autor}, {self._editorial}"
