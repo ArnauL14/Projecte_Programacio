bcac90b94dd754687e50bbbb1d30191f945a8b5c
VICTRX17 <1746098@uab.cat>
2025-05-25 12:08:37 +0200

    Update avaluador.py

--- avaluador.py
+++ avaluador.py
@@ -1,6 +1,6 @@
 import numpy as np
 from dataset import DatasetPelicules, DatasetLlibres, Dataset
-
+import logging
 
 class Avaluador:
     """
@@ -21,15 +21,22 @@ class Avaluador:
         Aquesta funció calcula i retorna el valor (float) del
         MAE (Mean Absolute Error), una mètrica per valorar la precisió de les prediccions.
         """
+        logging.info("Calculant MAE...")
+        if self._valoracins_reals.shape != self._valoracions.shape:
+            raise ValueError("Les matrius de valoracions reals i recomanades no tenen la mateixa forma.")
         return np.mean(np.abs(self._valoracins_reals - self._valoracions))
-    
+        logging.info("MAE calculat amb èxit.")
     def calcula_rmse(self):
         """
         Aquesta funció calcula i retorna el valorn (float) del
         RMSE , una mètrica per valorar la precisió de les prediccions
         """
+        logging.info("Calculant RMSE...")
+        if self._valoracins_reals.shape != self._valoracions.shape:
+            raise ValueError("Les matrius de valoracions reals i recomanades no tenen la mateixa forma.")
         mse = np.mean((self._valoracins_reals - self._valoracions) ** 2)
         return np.sqrt(mse)
+        logging.info("RMSE calculat amb èxit.")
     
 
 """dataset = DatasetPelicules("dataset/MovieLens100k/movies.csv", "dataset/MovieLens100k/ratings.csv")
